3388f11b37e504ffaa5dae6de08996ba936c4d6c
DonggunJung <donggun.dev@gmail.com>
2019-10-10 15:33:15 +0900

    update 3

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,23 @@ const router = Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const parser = excelParser('public/excel/dummy2.xlsx')
 
-  res.send(parser)
+  res.send(`
+    테스트를 하고 싶다면 Post 타입으로 "http://MY_URL/demo"를 호출하세요.   
+  `)
+});
+
+router.post('/demo', function(req, res, next) {
+  const workbook = excelParser('public/excel/dummy4(best).xls')
+  let worksheetIndex = workbook.SheetNames
+
+  return new Promise((resolve, reject) => {
+    worksheetIndex.map(name => {
+      resolve(workbook.Sheets[name])
+    })
+  }).then(sheetData => {
+    res.status(200).json({ sheetData })
+  })
 });
 
 module.exports = router;
